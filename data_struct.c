c88717e76f321083709714201ba566d24c3dde95
Sylvfo <you@example.com>
2024-12-11 17:44:42 +0100

    minilibx marche pas

---
+++ data_struct.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data_struct.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sforster <sforster@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 16:41:08 by sforster          #+#    #+#             */
+/*   Updated: 2024/12/10 16:41:08 by sforster         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// quelle type de data struct??
+
+
+// comment declarer les vecteurs et les points
+
+/*
+POLAR COORDINATE????
+
+
+un point C(2, -5) in 2D
+point P(5, -6, 8) in 3D
+
+int point[3];
+ｆｓｄｆｓｄｆｓｄｆｆｄｓ
+P[5, -6, 8]
+
+un vecteur 
+
+*/
+
+gdgfdfgＤｆｇ
+/*
